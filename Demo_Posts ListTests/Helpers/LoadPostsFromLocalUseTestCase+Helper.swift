d61aa9eb22c8d7e85e0d65ff32c009835c60d288
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-26 19:29:34 -0400

    feature/feedPosts/reorganizeTestHelpersFiles
    reorganize files to get most ordered code and must easy to read

---
+++ Demo_Posts ListTests/Helpers/LoadPostsFromLocalUseTestCase+Helper.swift
@@ -0,0 +1,62 @@
+//
+//  LoadPostsFromLocalUseTestCase+Helper.swift
+//  Demo_Posts ListTests
+//
+//  Created by Sonic on 26/5/23.
+//
+
+import XCTest
+@testable import Demo_Posts_List
+
+extension LoadPostsFromLocalUseTestCase {
+    
+    func makeSUT(fileName: String = "PostsList.json",
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalFeedLoader, reader: FileReaderSpy){
+        let reader = FileReaderSpy()
+        let sut = LocalFeedLoader(fileName: fileName, reader: reader)
+        return (sut, reader)
+    }
+    
+    func makePostsListJSON(_ items: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: items)
+    }
+    
+    func makePost() -> (model: FeedPost, json: [String: Any]) {
+        let item = FeedPost(name: "name", description: "description")
+        
+        let json = [
+            "name": item.name,
+            "description": item.description
+        ].compactMapValues{ $0 }
+        
+        return (item, json)
+    }
+    
+    func expect(_ sut: LocalFeedLoader,
+                          toCompleteWith expectedResult: Result<[FeedPost], LocalFeedLoader.Error>,
+                          when action: ()-> Void,
+                          file: StaticString = #filePath,
+                          line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            defer { exp.fulfill() }
+            
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+                
+            case let (.failure(receivedError as LocalFeedLoader.Error), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+        }
+        
+        action()
+        
+        waitForExpectations(timeout: 0.1)
+    }
+}
