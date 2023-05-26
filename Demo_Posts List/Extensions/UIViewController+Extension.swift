ea6b0c5be28edbcae8445e6933e1f4bb0b06e6c9
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-26 15:14:57 -0400

    feature/configurations/organize
    remove defaults call for main Storyboard. now the app start calling from SceneDelegate Programatically
    Add MVVM Folders for the pattern
    Create extension to call ViewConrtollers more simple and dinamically.
    Create a Constants values for StoryBoards Names

---
+++ Demo_Posts List/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Extension.swift
+//  Demo_Posts List
+//
+//  Created by Sonic on 26/5/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static func instantiate(from storyboardName: String) -> Self? {
+        let fullName = NSStringFromClass(self)
+        let className = fullName.components(separatedBy: ".")[1]
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
+        
+        return storyboard.instantiateViewController(withIdentifier: className) as? Self
+    }
+}
---
+++ Demo_Posts List/Views/PostsListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostsListViewController.swift
+//  Demo_Posts List
+//
+//  Created by Sonic on 26/5/23.
+//
+
+import UIKit
+
+final class PostsListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .secondarySystemBackground
+    }
+}
